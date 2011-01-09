211f2d5193295c9f42a5f18ab4a99d219c42b829
Dave Smith <dave@smithfamily.org.uk>
2011-01-09 21:42:46 +0000

    Add the font

--- src/uk/org/smithfamily/msparser/MSParser.java
+++ src/uk/org/smithfamily/msparser/MSParser.java
@@ -1,13 +1,27 @@
 package uk.org.smithfamily.msparser;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MSParser extends Activity {
+public class MSParser extends Activity
+{
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        setFont("fonts/ziska.ttf",R.id.text);
+    }
+
+    void setFont(String path, int res)
+    {
+        TextView name = (TextView) findViewById(res);
+        Typeface font = Typeface.createFromAsset(this.getAssets(), path);
+
+        name.setTypeface(font);
+
     }
 }
