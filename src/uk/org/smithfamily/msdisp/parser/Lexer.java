624a31d2097522e5e461776bf3d6ac4dcd1b953d
Dave Smith <dave@smithfamily.org.uk>
2011-03-25 15:29:17 +0000

    WIP

--- src/uk/org/smithfamily/msdisp/parser/Lexer.java
+++ src/uk/org/smithfamily/msdisp/parser/Lexer.java
@@ -5,32 +5,33 @@ import uk.org.smithfamily.msdisp.parser.functions.Functions;
 
 public class Lexer
 {
-    static final String DecimalNumeric = "0123456789";
-    static final String BinaryNumeric  = "01";
-    static final String HexNumeric     = "0123456789abcdefABCDEF";
-    static final String AlphaNumeric   = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_";
-    static final String Alpha          = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_";
+    private static final String INVALID_INPUT_TOKEN = "Invalid input token";
+    static final String         DecimalNumeric      = "0123456789";
+    static final String         BinaryNumeric       = "01";
+    static final String         HexNumeric          = "0123456789abcdefABCDEF";
+    static final String         AlphaNumeric        = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_";
+    static final String         Alpha               = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_";
 
-    static final char   AndChar        = '&';
-    static final char   OrChar         = '|';
-    static final char   PlusChar       = '+';
-    static final char   QuoteChar      = '"';
-    static final char   MinusChar      = '-';
-    static final char   MultChar       = '*';
-    static final char   ModChar        = '%';
-    static final char   DivChar        = '/';
-    static final char   LParenChar     = '(';
-    static final char   RParenChar     = ')';
-    static final char   CommaChar      = ',';
-    static final char   EqualChar      = '=';
-    static final char   LtChar         = '<';
-    static final char   GtChar         = '>';
-    static final char   NotChar        = '!';
-    static final char   QChar          = '?';
-    static final char   ColonChar      = ':';
-    static final char   CaretChar      = '^';
-    static final char   TildeChar      = '~';
-    static final char   TermChar       = '\0';
+    static final char           AndChar             = '&';
+    static final char           OrChar              = '|';
+    static final char           PlusChar            = '+';
+    static final char           QuoteChar           = '"';
+    static final char           MinusChar           = '-';
+    static final char           MultChar            = '*';
+    static final char           ModChar             = '%';
+    static final char           DivChar             = '/';
+    static final char           LParenChar          = '(';
+    static final char           RParenChar          = ')';
+    static final char           CommaChar           = ',';
+    static final char           EqualChar           = '=';
+    static final char           LtChar              = '<';
+    static final char           GtChar              = '>';
+    static final char           NotChar             = '!';
+    static final char           QChar               = '?';
+    static final char           ColonChar           = ':';
+    static final char           CaretChar           = '^';
+    static final char           TildeChar           = '~';
+    static final char           TermChar            = '\0';
 
     enum Token
     {
@@ -38,61 +39,54 @@ public class Lexer
     };
 
     private ExprError e;
-    private String    inputLine  = "";
-    //private int       nextChar;
+    private String    inputLine      = "";
+    // private int nextChar;
 
-    private int nextCharPos = 1;
+    private int       currentCharPos = 0;
     public Token      CurrentTok;
     Function          CurrentFnc;
     public int        fnArgCount;
     public int        CurrentVar;
     public Symbol     CurrentSym;
-    public String     CurrentStr = "";
+    public String     CurrentStr     = "";
     public int        CurrentInt;
     public double     CurrentDbl;
 
-    char currentChar()
-    {
-        return inputLine.charAt(nextCharPos-1);
-    }
-    char nextChar() throws ExprError
-    {
-        if (nextCharPos > inputLine.length()-1)
-        {
-            e.Error("Attempt to move beyond length of input: input='"+inputLine+"'");
-        }
-        return inputLine.charAt(nextCharPos++);
-    }
-    String GetSequence(char firstChar, String validChars)
+    String GetSequence(char firstChar, String validChars) throws ExprError
     {
         String result = "";
         char inputChar = firstChar;
         int inputLoc = 0;
         int itsThere = validChars.indexOf(inputChar);
-        
-        do
+
+        while(currentCharPos < inputLine.length() && itsThere >= 0)
         {
             result += inputChar;
             inputLoc++;
-            if(nextChar < inputLine.length())
+            currentCharPos++;
+            if (currentCharPos < inputLine.length())
             {
-                inputChar = inputLine.charAt(nextChar++);
-                itsThere = validChars.indexOf(inputChar);   
+                inputChar = inputLine.charAt(currentCharPos);
+                itsThere = validChars.indexOf(inputChar);
+            }
+            else
+            {
+                break;
             }
         }
-        while (nextChar < inputLine.length() && itsThere >= 0);
+        
         return result;
     }
 
-    String GetUntil(char untilChar)
+    String GetUntil(char untilChar) throws ExprError
     {
         String destStr = "";
-
-        while (inputLine.charAt(nextChar) != untilChar)
+        testPosition();
+        while (currentCharPos < inputLine.length() && inputLine.charAt(currentCharPos) != untilChar)
         {
-            destStr += inputLine.charAt(nextChar++);
+            destStr += inputLine.charAt(currentCharPos++);
         }
-        inputLine.charAt(nextChar++); // Past the closing one
+        currentCharPos++;
 
         return destStr;
     }
@@ -106,18 +100,18 @@ public class Lexer
         int base = 10;
         String Numeric = Lexer.DecimalNumeric;
 
-        if (firstChar == '0' && inputLine.length() > nextChar)
+        if (firstChar == '0' && inputLine.length() > currentCharPos + 1)
         { // Allow for binary numbers with a variant on C's hex: 0b10101
-            switch (inputLine.charAt(nextChar))
+            switch (inputLine.charAt(currentCharPos + 1))
             {
             case 'b':
                 base = 2;
-                inputLine.charAt(nextChar++);
+                currentCharPos++;
                 Numeric = BinaryNumeric;
                 break;
             case 'x':
                 base = 16;
-                inputLine.charAt(nextChar++);
+                currentCharPos++;
                 Numeric = HexNumeric;
                 break;
             }
@@ -125,71 +119,93 @@ public class Lexer
 
         numStr = GetSequence(firstChar, Numeric);
 
-        inputChar = inputLine.charAt(nextChar);
-        if (".eE".indexOf(inputChar) < 0)
+        if (currentCharPos < inputLine.length())// There's more to process
         {
-            // DiagPrint("   Integer value: >%s<\n", numStr);
-            CurrentTok = Token.IntTok;
-            CurrentInt = Integer.valueOf(numStr, base);
-            // if (*end)
-            // e.Error("Numeric conversion error, value may have overflowed");
-        } else
-        {
-            if (base != 10)
-                e.Error("Floating point values may only be base 10");
-
-            if (inputChar == '.')
+            inputChar = inputLine.charAt(currentCharPos);
+            if (".eE".indexOf(inputChar) < 0)
             {
-                numStr += ".";
-                inputLine.charAt(nextChar++);
-                ;
-                inputChar = inputLine.charAt(nextChar++);
-                ;
+                // DiagPrint("   Integer value: >%s<\n", numStr);
+                CurrentTok = Token.IntTok;
+                CurrentInt = Integer.valueOf(numStr, base);
+                return;
+                // if (*end)
+                // e.Error("Numeric conversion error, value may have overflowed");
+            }
+            else
+            {
+                if (base != 10)
+                    e.Error("Floating point values may only be base 10");
 
-                newStr = GetSequence(inputChar, DecimalNumeric);
-                if (newStr != null)
-                    numStr += newStr;
-                else
+                if (inputChar == '.')
                 {
-                    e.backup();
-                    nextChar--;
+                    numStr += ".";
+                    moveForward();
+                    inputChar = inputLine.charAt(currentCharPos++);
+
+                    newStr = GetSequence(inputChar, DecimalNumeric);
+                    if (newStr != null)
+                        numStr += newStr;
+                    else
+                    {
+                        e.backup();
+                        currentCharPos--;
+                    }
+                }
+                if (numStr.length() == 1 && numStr.charAt(0) == '.')
+                    e.Error("Expected numeric digits after decimal");
+
+                
+                if(currentCharPos < inputLine.length())
+                {
+                    testPosition();
+                    inputChar = inputLine.charAt(currentCharPos++);
+                    
+                    if ("Ee".indexOf(inputChar) >= 0)
+                    {
+                        numStr += 'e';
+                        testPosition();
+                        inputChar = inputLine.charAt(currentCharPos++);
+    
+                        if (inputChar == '+' || inputChar == '-')
+                        {
+                            if (inputChar == '-')
+                                numStr += "-";
+                            testPosition();
+                            inputChar = inputLine.charAt(currentCharPos++);
+                        }
+    
+                        newStr = GetSequence(inputChar, DecimalNumeric);
+                        numStr += newStr;
+                        if (newStr.length() == 0)
+                            e.Error("Expected number for exponent");
+                        testPosition();
+                        inputChar = inputLine.charAt(currentCharPos++);
+    
+                    }
                 }
             }
-            if (numStr.length() == 1 && numStr.charAt(0) == '.')
-                e.Error("Expected numeric digits after decimal");
-
-            inputChar = inputLine.charAt(nextChar++);
-            ;
-            if ("Ee".indexOf(inputChar) >= 0)
-            {
-                numStr += 'e';
-                inputChar = inputLine.charAt(nextChar++);
-                ;
-
-                if (inputChar == '+' || inputChar == '-')
-                {
-                    if (inputChar == '-')
-                        numStr += "-";
-                    inputChar = inputLine.charAt(nextChar++);
-                    ;
-                }
-
-                newStr = GetSequence(inputChar, DecimalNumeric);
-                numStr += newStr;
-                if (newStr.length() == 0)
-                    e.Error("Expected number for exponent");
-                inputChar = inputLine.charAt(nextChar++);
-                ;
-            }
-
+         
             e.backup();
-            nextChar--;
+            currentCharPos--;
 
             // DiagPrint("   Real value: >%s<\n", numStr);
             CurrentTok = Token.DblTok;
             CurrentDbl = Double.valueOf(numStr);
             // if (*end)
             // e.Error("Numeric conversion error, value may have overflowed");
+        }
+        else
+        {
+            CurrentTok = Token.IntTok;
+            CurrentInt = Integer.valueOf(numStr, base);
+        }
+    }
+
+    private void testPosition() throws ExprError
+    {
+        if (currentCharPos >= inputLine.length())
+        {
+            e.Error(INVALID_INPUT_TOKEN);
         }
     }
 
@@ -201,15 +217,20 @@ public class Lexer
     void Init(String Line)
     {
         inputLine += Line;
-        nextChar = 0;
+        currentCharPos = 0;
     }
 
     void GetToken() throws ExprError
     {
-        while (Character.isWhitespace((inputLine.charAt(nextChar))))
-            nextChar++;
+        if (currentCharPos >= inputLine.length())
+        {
+            CurrentTok = Token.TermTok;
+            return;
+        }
+        while (currentCharPos < inputLine.length() && Character.isWhitespace(inputLine.charAt(currentCharPos)))
+            currentCharPos++;
 
-        char InputChar = inputLine.charAt(nextChar++);
+        char InputChar = inputLine.charAt(currentCharPos);
         ;
         e.reset(InputChar);
 
@@ -223,7 +244,8 @@ public class Lexer
             {
                 CurrentTok = Token.FunctionTok;
                 fnArgCount = CurrentFnc.argCnt();
-            } else
+            }
+            else
             {
                 // Search the variables table in controller symbol table; if not
                 // there, error.
@@ -239,63 +261,79 @@ public class Lexer
                     CurrentTok = Token.ConstantTok;
                 }
             }
-        } else if (DecimalNumeric.indexOf(InputChar) >= 0)
+        }
+        else if (DecimalNumeric.indexOf(InputChar) >= 0)
         {
             GetNumber(InputChar);
-        } else
+        }
+        else
         {
             switch (InputChar)
             {
             case QuoteChar:
                 CurrentStr = GetUntil(QuoteChar);
                 CurrentTok = Token.StrTok;
+                moveForward();
                 break;
             case PlusChar:
                 CurrentTok = Token.PlusTok;
+                moveForward();
                 break;
             case MinusChar:
                 CurrentTok = Token.MinusTok;
+                moveForward();
                 break;
             case MultChar:
                 CurrentTok = Token.MultTok;
+                moveForward();
                 break;
             case ModChar:
                 CurrentTok = Token.ModTok;
+                moveForward();
                 break;
             case DivChar:
                 CurrentTok = Token.DivTok;
+                moveForward();
                 break;
             case LParenChar:
                 CurrentTok = Token.LParenTok;
+                moveForward();
                 break;
             case RParenChar:
                 CurrentTok = Token.RParenTok;
+                moveForwardNoTest();
                 break;
             case CommaChar:
                 CurrentTok = Token.CommaTok;
+                moveForward();
                 break;
             case TermChar:
                 CurrentTok = Token.TermTok;
+                moveForward();
                 break;
             case CaretChar:
                 CurrentTok = Token.BXorTok;
+                moveForward();
                 break;
             case TildeChar:
                 CurrentTok = Token.BNotTok;
+                moveForward();
                 break;
             case QChar:
                 CurrentTok = Token.CondTok;
+                moveForward();
                 break;
             case ColonChar:
                 CurrentTok = Token.ColonTok;
+                moveForward();
                 break;
             case AndChar:
-                switch (inputLine.charAt(nextChar))
+                moveForward();
+                switch (inputLine.charAt(currentCharPos))
                 {
                 case AndChar:
                     CurrentTok = Token.LAndTok;
-                    inputLine.charAt(nextChar++);
-                    ;
+                    currentCharPos++;
                     break;
                 default:
                     CurrentTok = Token.BAndTok;
@@ -303,12 +341,12 @@ public class Lexer
                 }
                 break;
             case OrChar:
-                switch (inputLine.charAt(nextChar))
+                moveForward();
+                switch (inputLine.charAt(currentCharPos))
                 {
                 case OrChar:
                     CurrentTok = Token.LOrTok;
-                    inputLine.charAt(nextChar++);
-                    ;
+                    currentCharPos++;
                     break;
                 default:
                     CurrentTok = Token.BOrTok;
@@ -316,12 +354,12 @@ public class Lexer
                 }
                 break;
             case NotChar:
-                switch (inputLine.charAt(nextChar))
+                moveForward();
+                switch (inputLine.charAt(currentCharPos))
                 {
                 case EqualChar:
                     CurrentTok = Token.NeTok;
-                    inputLine.charAt(nextChar++);
-                    ;
+                    currentCharPos++;
                     break;
                 default:
                     CurrentTok = Token.LNotTok;
@@ -329,12 +367,12 @@ public class Lexer
                 }
                 break;
             case EqualChar:
-                switch (inputLine.charAt(nextChar))
+                moveForward();
+                switch (inputLine.charAt(currentCharPos))
                 {
                 case EqualChar:
                     CurrentTok = Token.EqTok;
-                    inputLine.charAt(nextChar++);
-                    ;
+                    currentCharPos++;
                     break;
                 default:
                     CurrentTok = Token.EqualTok;
@@ -342,17 +380,15 @@ public class Lexer
                 }
                 break;
             case LtChar:
-                switch (inputLine.charAt(nextChar))
+                moveForward();
+                switch (inputLine.charAt(currentCharPos))
                 {
                 case LtChar:
-                    CurrentTok = Token.LShiftTok;
-                    inputLine.charAt(nextChar++);
-                    ;
+                    currentCharPos++;
                     break;
                 case EqualChar:
                     CurrentTok = Token.LeTok;
-                    inputLine.charAt(nextChar++);
-                    ;
+                    currentCharPos++;
                     break;
                 default:
                     CurrentTok = Token.LtTok;
@@ -360,17 +396,16 @@ public class Lexer
                 }
                 break;
             case GtChar:
-                switch (inputLine.charAt(nextChar))
+                moveForward();
+                switch (inputLine.charAt(currentCharPos))
                 {
                 case GtChar:
                     CurrentTok = Token.RShiftTok;
-                    inputLine.charAt(nextChar++);
-                    ;
+                    currentCharPos++;
                     break;
                 case EqualChar:
                     CurrentTok = Token.GeTok;
-                    inputLine.charAt(nextChar++);
-                    ;
+                    currentCharPos++;
                     break;
                 default:
                     CurrentTok = Token.GtTok;
@@ -378,10 +413,23 @@ public class Lexer
                 }
                 break;
             default:
-                e.Error("Invalid input token");
+                e.Error(INVALID_INPUT_TOKEN);
                 break;
             }
         }
+    }
+
+    private void moveForwardNoTest()
+    {
+        
+        currentCharPos++;
+        
+    }
+
+    private void moveForward() throws ExprError
+    {
+        currentCharPos++;
+        testPosition();
     }
 
 }
