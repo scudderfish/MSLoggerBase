221790b7177826ec88ac0ce53619c0566605afa5
Dave Smith <dave.g.smith@gmail.com>
2011-10-10 13:08:15 +0100

    Flush the buffers when dealing with multiple files

--- src/Normaliser.java
+++ src/Normaliser.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -6,10 +8,13 @@ import org.apache.commons.lang3.StringUtils;
 
 public class Normaliser
 {
-	private static Pattern	bits	= Pattern.compile("(\\w*)\\s*=\\s*bits,\\s*(.*),\\s*(.*),\\s*\\[(\\d):(\\d)\\].*");
-	private static Pattern	scalar	= Pattern.compile("(\\w*)\\s*=\\s*scalar,\\s*([U|S]\\d+),\\s*(\\d+),\\s*.*,.*");
-	private static Pattern	expr	= Pattern.compile("(\\w*)\\s*=\\s*\\{\\s*(.*)\\s*\\}.*");
-	private static Pattern	ternary	= Pattern.compile("(.*)\\((.*)\\?(.*)");
+	private static Pattern		bits		= Pattern.compile("(\\w*)\\s*=\\s*bits,\\s*(.*),\\s*(.*),\\s*\\[(\\d):(\\d)\\].*");
+	private static Pattern		scalar		= Pattern.compile("(\\w*)\\s*=\\s*scalar,\\s*([U|S]\\d+),\\s*(\\d+),\\s*.*,.*");
+	private static Pattern		expr		= Pattern.compile("(\\w*)\\s*=\\s*\\{\\s*(.*)\\s*\\}.*");
+	private static Pattern		ternary		= Pattern.compile("(.*)\\((.*)\\?(.*)");
+	private static List<String>	runtimeVars	= new ArrayList<String>();
+	private static List<String>	expressions	= new ArrayList<String>();
+	private static List<String>	logEntries	= new ArrayList<String>();
 
 	/**
 	 * @param args
@@ -25,6 +30,10 @@ public class Normaliser
 
 	private static void process(String filename) throws IOException
 	{
+		runtimeVars = new ArrayList<String>();
+		expressions = new ArrayList<String>();
+		logEntries = new ArrayList<String>();
+
 		boolean processing = false;
 		BufferedReader br = new BufferedReader(new FileReader(filename));
 
@@ -45,12 +54,27 @@ public class Normaliser
 			if (processing)
 			{
 				processText(line);
+
 			}
 		}
+		PrintWriter writer = new PrintWriter(new FileWriter(filename + ".jd"));
+
+		for (String defn : runtimeVars)
+		{
+			writer.println(defn);
+			// System.out.println(defn);
+		}
+		for (String defn : expressions)
+		{
+			writer.println(defn);
+			// System.out.println(defn);
+		}
+		writer.close();
 	}
 
 	private static void processText(String line)
 	{
+		String definition = null;
 		line = StringUtils.trim(line).split(";")[0];
 		if (StringUtils.isEmpty(line))
 		{
@@ -59,28 +83,67 @@ public class Normaliser
 		Matcher bitsM = bits.matcher(line);
 		Matcher scalarM = scalar.matcher(line);
 		Matcher exprM = expr.matcher(line);
-		if(bitsM.matches())
+		if (bitsM.matches())
 		{
 			String name = bitsM.group(1);
-			String a = bitsM.group(2);
-			String b = bitsM.group(3);
-			String c = bitsM.group(4);
-			String d = bitsM.group(5);
-			System.out.println(name+" = getBits(ochBuffer,"+b+","+c+","+d+");");
-			}
-		else if(scalarM.matches())
+			String offset = bitsM.group(3);
+			String start = bitsM.group(4);
+			String end = bitsM.group(5);
+			definition = (name + " = getBits(ochBuffer," + offset + "," + start + "," + end + ");");
+			runtimeVars.add(definition);
+		}
+		else if (scalarM.matches())
 		{
-			
+			String name = scalarM.group(1);
+			String dataType = scalarM.group(2);
+			String offset = scalarM.group(3);
+			definition = getScalar(name, dataType, offset);
+			runtimeVars.add(definition);
 		}
 		else if (exprM.matches())
 		{
-			
+			String name = exprM.group(1);
+			String expression = exprM.group(2).trim();
+			Matcher ternaryM = ternary.matcher(expression);
+			if (ternaryM.matches())
+			{
+				expression = "((" + ternaryM.group(2) + " != 0 ) ? " + ternaryM.group(3);
+			}
+			definition = name + " = (" + expression + ")";
+			expressions.add(definition);
 		}
 		else
 		{
-			System.out.println("// " + line);	
+			// System.out.println("// " + line);
 		}
-		
+	}
+
+	private static String getScalar(String name, String dataType, String offset)
+	{
+		String definition = name + " = get";
+		if (dataType.startsWith("S"))
+		{
+			definition += "Signed";
+		}
+		int size = Integer.parseInt(dataType.substring(1));
+		switch (size)
+		{
+		case 8:
+			definition += "Byte";
+			break;
+		case 16:
+			definition += "Word";
+			break;
+		case 32:
+			definition += "Long";
+			break;
+		default:
+			definition += dataType;
+			break;
+
+		}
+		definition += "(ochBuffer," + offset + ");";
+		return definition;
 	}
 
 }
