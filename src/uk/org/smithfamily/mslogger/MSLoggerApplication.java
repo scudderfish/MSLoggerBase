b079a2b51f2109c3d36b225f08b02a05569dbcc3
Dave Smith <dave.g.smith@gmail.com>
2011-10-15 09:16:07 +0100

    Post crash results via acra

--- src/uk/org/smithfamily/mslogger/MSLoggerApplication.java
+++ src/uk/org/smithfamily/mslogger/MSLoggerApplication.java
@@ -1,18 +1,21 @@
 package uk.org.smithfamily.mslogger;
 
-import org.acra.ACRA;
+import org.acra.*;
 import org.acra.annotation.ReportsCrashes;
 
 import android.app.Application;
 
-@ReportsCrashes(formUri = "http://www.bugsense.com/api/acra?api_key=fd6951fe", formKey = "")
+// "http://www.bugsense.com/api/acra?api_key=fd6951fe"
+@ReportsCrashes(formKey = "dDZmcjZfU0xVVTN6b2ZtTkdjM2pGY1E6MQ", logcatArguments = { "-t", "200", "-v", "time" }, 
+        mode = ReportingInteractionMode.TOAST, forceCloseDialogAfterToast = false,                                                                                                          // false
+resToastText = R.string.crash_toast_text)
 public class MSLoggerApplication extends Application
 {
-	@Override
-	public void onCreate()
-	{
-		// The following line triggers the initialization of ACRA
-		ACRA.init(this);
-		super.onCreate();
-	}
+    @Override
+    public void onCreate()
+    {
+        // The following line triggers the initialization of ACRA
+        ACRA.init(this);
+        super.onCreate();
+    }
 }
