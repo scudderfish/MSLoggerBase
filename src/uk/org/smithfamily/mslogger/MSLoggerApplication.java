9ddc60e10c51d6d1232d024977a9de3a63bfde45
Dave Smith <dave@smithfamily.org.uk>
2011-10-16 11:03:02 +0100

    Send crash reports by email

--- src/uk/org/smithfamily/mslogger/MSLoggerApplication.java
+++ src/uk/org/smithfamily/mslogger/MSLoggerApplication.java
@@ -1,14 +1,31 @@
 package uk.org.smithfamily.mslogger;
 
-import org.acra.*;
+import org.acra.ACRA;
+import org.acra.ReportingInteractionMode;
 import org.acra.annotation.ReportsCrashes;
+import org.acra.*;
 
 import android.app.Application;
 
 // "http://www.bugsense.com/api/acra?api_key=fd6951fe"
-@ReportsCrashes(formKey = "dDZmcjZfU0xVVTN6b2ZtTkdjM2pGY1E6MQ", logcatArguments = { "-t", "200", "-v", "time" }, 
-        mode = ReportingInteractionMode.TOAST, forceCloseDialogAfterToast = false,                                                                                                          // false
-resToastText = R.string.crash_toast_text)
+//https://docs.google.com/spreadsheet/viewform?formkey=dGFDbHRzU1M1UFJvTTVKNlZDLTlhSGc6MQ
+@ReportsCrashes(//formKey = "dGFDbHRzU1M1UFJvTTVKNlZDLTlhSGc6MQ",
+            formKey="",
+            mailTo="dave.g.smith@gmail.com",
+            customReportContent = { ReportField.APP_VERSION_NAME, 
+                        ReportField.ANDROID_VERSION, 
+                        ReportField.PHONE_MODEL, 
+                        ReportField.CUSTOM_DATA, 
+                        ReportField.STACK_TRACE, 
+                        ReportField.LOGCAT,
+                        ReportField.DEVICE_FEATURES,
+                        ReportField.RADIOLOG,
+                        ReportField.SETTINGS_SYSTEM,
+                        ReportField.SHARED_PREFERENCES}, 
+            logcatArguments = { "-t", "200", "-v", "time" }, 
+            mode = ReportingInteractionMode.TOAST, 
+            forceCloseDialogAfterToast = true, 
+            resToastText = R.string.crash_toast_text)
 public class MSLoggerApplication extends Application
 {
     @Override
