e07fe42d140375c6dc43f690c6d3e2ccbbcc743f
Dave Smith <dave@smithfamily.org.uk>
2011-10-20 20:40:17 +0100

    Send by email a zip file when the 'Stop Logging' button is pressed

--- src/uk/org/smithfamily/mslogger/log/EmailManager.java
+++ src/uk/org/smithfamily/mslogger/log/EmailManager.java
@@ -1,42 +1,31 @@
 package uk.org.smithfamily.mslogger.log;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
 
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 
-public enum EmailManager
+public class EmailManager
 {
-	INSTANCE;
-
-	public void sendLogs()
-	{
-
-		
-		
-	}
-
 	public static void email(Context context, String emailTo, String emailCC, String subject, String emailText,
 			List<String> filePaths)
 	{
 		// need to "send multiple" to get more than one attachment
-		final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND_MULTIPLE);
-		emailIntent.setType("plain/text");
+		final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+		emailIntent.setType("application/zip");
 		emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[] { emailTo });
 		emailIntent.putExtra(android.content.Intent.EXTRA_CC, new String[] { emailCC });
-		// has to be an ArrayList
-		ArrayList<Uri> uris = new ArrayList<Uri>();
-		// convert from paths to Android friendly Parcelable Uri's
-		for (String file : filePaths)
-		{
-			File fileIn = new File(file);
-			Uri u = Uri.fromFile(fileIn);
-			uris.add(u);
-		}
-		emailIntent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, uris);
+		emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+		emailIntent.putExtra(Intent.EXTRA_TEXT, emailText);
+        
+		File zipFile = new File(filePaths.get(0)+".zip");
+		Compress c = new Compress(filePaths,zipFile.getAbsolutePath());
+		
+		c.zip();
+	
+		emailIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(zipFile));
 		context.startActivity(Intent.createChooser(emailIntent, "Send mail..."));
 	}
 }
