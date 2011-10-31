877efce216babadaa42b59adaf88b39b754c7f73
Dave Smith <dave.g.smith@gmail.com>
2011-10-31 20:41:26 +0000

    Only send emails if we have some files to email

--- src/uk/org/smithfamily/mslogger/log/EmailManager.java
+++ src/uk/org/smithfamily/mslogger/log/EmailManager.java
@@ -1,6 +1,7 @@
 package uk.org.smithfamily.mslogger.log;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 import android.content.Context;
@@ -9,26 +10,42 @@ import android.net.Uri;
 
 public class EmailManager
 {
-	public static void email(Context context, String emailTo, String emailCC, String subject, String emailText,
-			List<String> filePaths)
-	{
-		// need to "send multiple" to get more than one attachment
-		final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
-		emailIntent.setType("application/zip");
-		emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[] { emailTo });
-		if (emailCC != null)
-		{
-			emailIntent.putExtra(android.content.Intent.EXTRA_CC, new String[] { emailCC });
-		}
-		emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
-		emailIntent.putExtra(Intent.EXTRA_TEXT, emailText);
+    public static void email(Context context, String emailTo, String emailCC, String subject, String emailText,
+            List<String> filePaths)
+    {
+        List<String> actualFiles = new ArrayList<String>();
 
-		File zipFile = new File(filePaths.get(0) + ".zip");
-		Compress c = new Compress(filePaths, zipFile.getAbsolutePath());
+        for (String name : filePaths)
+        {
+            if (name != null)
+            {
+                File f = new File(name);
 
-		c.zip();
+                if (f.exists() && f.canRead())
+                    actualFiles.add(name);
+            }
+        }
+        if (actualFiles.size() == 0)
+        {
+            return;
+        }
+        // need to "send multiple" to get more than one attachment
+        final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+        emailIntent.setType("application/zip");
+        emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[] { emailTo });
+        if (emailCC != null)
+        {
+            emailIntent.putExtra(android.content.Intent.EXTRA_CC, new String[] { emailCC });
+        }
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        emailIntent.putExtra(Intent.EXTRA_TEXT, emailText);
 
-		emailIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(zipFile));
-		context.startActivity(Intent.createChooser(emailIntent, "Send mail..."));
-	}
+        File zipFile = new File(filePaths.get(0) + ".zip");
+        Compress c = new Compress(filePaths, zipFile.getAbsolutePath());
+
+        c.zip();
+
+        emailIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(zipFile));
+        context.startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+    }
 }
