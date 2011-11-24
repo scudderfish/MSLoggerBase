3b773b3eac171330608c77b045ca93c7c6e6cd7b
Dave Smith <dave@smithfamily.org.uk>
2011-11-24 19:18:16 +0000

    Fix a crash if the user selects "No paired devices" as their device.

--- MSLoggerCore/src/uk/org/smithfamily/mslogger/activity/DeviceListActivity.java
+++ MSLoggerCore/src/uk/org/smithfamily/mslogger/activity/DeviceListActivity.java
@@ -165,13 +165,18 @@ public class DeviceListActivity extends Activity
                                                          {
                                                              public void onItemClick(AdapterView<?> av, View v, int arg2, long arg3)
                                                              {
+                                                            	 String info = ((TextView) v).getText().toString();
+                                                                 String address = info.substring(info.length() - 17);
+                                                                 if(!BluetoothAdapter.checkBluetoothAddress(address))
+                                                                 {
+                                                                	 return;
+                                                                 }
+                                                                 
                                                                  // Cancel discovery because it's costly and we're about to connect
                                                                  mBtAdapter.cancelDiscovery();
 
                                                                  // Get the device MAC address, which is the last 17 chars in the View
-                                                                 String info = ((TextView) v).getText().toString();
-                                                                 String address = info.substring(info.length() - 17);
-
+                                                                 
                                                                  // Create the result Intent and include the MAC address
                                                                  Intent intent = new Intent();
                                                                  intent.putExtra(EXTRA_DEVICE_ADDRESS, address);
