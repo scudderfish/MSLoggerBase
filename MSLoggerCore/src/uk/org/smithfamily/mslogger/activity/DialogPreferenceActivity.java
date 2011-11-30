6272f9f7487327bc7abbcb980d111ffe793c494a
Dave Smith <dave@smithfamily.org.uk>
2011-11-30 21:17:13 +0000

    Formalise initial configuration

---
+++ MSLoggerCore/src/uk/org/smithfamily/mslogger/activity/DialogPreferenceActivity.java
@@ -0,0 +1,43 @@
+package uk.org.smithfamily.mslogger.activity;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.view.*;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class DialogPreferenceActivity extends PreferenceActivity
+{
+
+	protected void addButton()
+	{
+		LinearLayout v = new LinearLayout(this);
+		Button b = new Button(this);
+		b.setText("OK");
+		b.setGravity(Gravity.CENTER_HORIZONTAL);
+		LayoutParams p = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT);
+
+		b.setLayoutParams(p);
+		b.setOnClickListener(new OnClickListener()
+		{
+
+			@Override
+			public void onClick(View v)
+			{
+				finish();
+			}
+		});
+		v.addView(b);
+		this.getListView().addFooterView(v);
+	}
+
+	public void onCreate(Bundle savedInstanceState, int preferences)
+	{
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(preferences);
+		addButton();
+	}
+
+}
