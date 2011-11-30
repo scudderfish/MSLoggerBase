cdf2ae3226b9518670c85de8a616cd078ccb3cb7
Dave Smith <dave@smithfamily.org.uk>
2011-11-30 21:11:02 +0000

    Factor out socket generation

---
+++ MSLoggerCore/src/uk/org/smithfamily/mslogger/comms/BTSocketFactory.java
@@ -0,0 +1,64 @@
+package uk.org.smithfamily.mslogger.comms;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.UUID;
+
+import uk.org.smithfamily.mslogger.ApplicationSettings;
+import uk.org.smithfamily.mslogger.log.DebugLogManager;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+
+public class BTSocketFactory
+{
+	private static UUID	RFCOMM_UUID	= UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+	public static BluetoothSocket getSocket(BluetoothDevice device)
+	{
+		BluetoothSocket tmp = null;
+		if (ApplicationSettings.INSTANCE.isBTWorkaround())
+		{
+			try
+			{
+				tmp = createWorkaroundSocket(device);
+			}
+			catch (Exception e)
+			{
+				DebugLogManager.INSTANCE.logException(e);
+			}
+		}
+		else
+		{
+			try
+			{
+				tmp = device.createRfcommSocketToServiceRecord(RFCOMM_UUID);
+			}
+			catch (IOException e)
+			{
+				// That didn't work, let's try the work around
+				DebugLogManager.INSTANCE.logException(e);
+
+				try
+				{
+					tmp = createWorkaroundSocket(device);
+					// That did work, let's make it permanment
+					ApplicationSettings.INSTANCE.setBTWorkaround(true);
+				}
+				catch (Exception e1)
+				{
+					// We're boned, nothing works
+					DebugLogManager.INSTANCE.logException(e);
+				}
+			}
+		}
+		return tmp;
+	}
+
+	private static BluetoothSocket createWorkaroundSocket(BluetoothDevice device) throws Exception
+	{
+		Method m = device.getClass().getMethod("createRfcommSocket", new Class[] { int.class });
+		BluetoothSocket tmp = (BluetoothSocket) m.invoke(device, Integer.valueOf(1));
+		return tmp;
+	}
+
+}
--- MSLoggerCore/src/uk/org/smithfamily/mslogger/ecuDef/ECUFingerprint.java
+++ MSLoggerCore/src/uk/org/smithfamily/mslogger/ecuDef/ECUFingerprint.java
@@ -1,289 +1,285 @@
 package uk.org.smithfamily.mslogger.ecuDef;
 
-import java.io.*;
-import java.util.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import uk.org.smithfamily.mslogger.ApplicationSettings;
 import uk.org.smithfamily.mslogger.MSLoggerApplication;
+import uk.org.smithfamily.mslogger.comms.BTSocketFactory;
 import uk.org.smithfamily.mslogger.log.DebugLogManager;
-import android.bluetooth.*;
-import android.content.Context;
-import android.os.*;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 
 public class ECUFingerprint implements Runnable
 {
 
-    private static final String TAG         = "ECUFingerprint";
-    public static final String  UNKNOWN     = "UNKNOWN";
-    private UUID                RFCOMM_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
-    private InputStream         mmInStream;
-    private OutputStream        mmOutStream;
-    private BluetoothSocket     socket;
+	private static final String	TAG		= "ECUFingerprint";
+	public static final String	UNKNOWN	= "UNKNOWN";
+	private InputStream			mmInStream;
+	private OutputStream		mmOutStream;
+	private BluetoothSocket		socket;
 
-    private boolean             located     = false;
-    private Context             context;
-    private Handler             handler;
-    private BluetoothAdapter    adapter;
+	private boolean				located	= false;
+	private Handler				handler;
+	private BluetoothAdapter	adapter;
 
-    public ECUFingerprint(Context c, Handler h, BluetoothAdapter mBluetoothAdapter)
-    {
-        this.adapter = mBluetoothAdapter;
-        this.context = c;
-        this.handler = h;
-    }
+	public ECUFingerprint(Handler h, BluetoothAdapter mBluetoothAdapter)
+	{
+		this.adapter = mBluetoothAdapter;
+		this.handler = h;
+	}
 
-    private void sendStatus(String msgStr)
-    {
-        Message msg = handler.obtainMessage(MSLoggerApplication.MESSAGE_TOAST);
-        Bundle bundle = new Bundle();
-        bundle.putString(MSLoggerApplication.MSG_ID, msgStr);
-        msg.setData(bundle);
-        handler.sendMessage(msg);
+	private void sendStatus(String msgStr)
+	{
+		Message msg = handler.obtainMessage(MSLoggerApplication.MESSAGE_TOAST);
+		Bundle bundle = new Bundle();
+		bundle.putString(MSLoggerApplication.MSG_ID, msgStr);
+		msg.setData(bundle);
+		handler.sendMessage(msg);
 
-    }
+	}
 
-    @Override
-    public void run()
-    {
-        Thread.currentThread().setName("ECUFingerprintThread");
-        String fingerprint = UNKNOWN;
-        while (!located)
-        {
+	@Override
+	public void run()
+	{
+		Thread.currentThread().setName("ECUFingerprintThread");
+		String fingerprint = UNKNOWN;
+		while (!located)
+		{
+			String btAddr = ApplicationSettings.INSTANCE.getBluetoothMac();
+			sendStatus("Attempting to connect");
+			BluetoothDevice remote = adapter.getRemoteDevice(btAddr);
+			socket = BTSocketFactory.getSocket(remote);
 
-            String btAddr = ApplicationSettings.INSTANCE.getBluetoothMac();
-            sendStatus("Attempting to connect");
-            BluetoothDevice remote = adapter.getRemoteDevice(btAddr);
-            try
-            {
-                socket = remote.createRfcommSocketToServiceRecord(RFCOMM_UUID);
-            }
-            catch (IOException e)
-            {
-                DebugLogManager.INSTANCE.logException(e);
-                sendStatus("Connection failed");
-                Log.e(TAG, "create() failed", e);
-            }
-            adapter.cancelDiscovery();
-            try
-            {
-                // This is a blocking call and will only return on a
-                // successful connection or an exception
-                socket.connect();
-                sendStatus("Connected");
-            }
-            catch (IOException e)
-            {
-                sendStatus("Connection failed");
+			adapter.cancelDiscovery();
 
-                // ErrorReporter.getInstance().handleException(e);
-                DebugLogManager.INSTANCE.logException(e);
-                delay(1000);
+			try
+			{
+				// This is a blocking call and will only return on a
+				// successful connection or an exception
+				socket.connect();
+				sendStatus("Connected");
+			}
+			catch (IOException e)
+			{
+				sendStatus("Connection failed");
 
-                // Close the socket
-                try
-                {
-                    socket.close();
-                }
-                catch (IOException e2)
-                {
-                    // ErrorReporter.getInstance().handleException(e2);
-                    Log.e(TAG, "unable to close() socket during connection failure", e2);
-                }
-            }
+				// ErrorReporter.getInstance().handleException(e);
+				DebugLogManager.INSTANCE.logException(e);
+				delay(1000);
 
-            try
-            {
-                fingerprint = getFingerprint(socket, remote);
-                located = true;
-            }
-            catch (IOException e)
-            {
-                tearDown();
-                delay(1000);
-            }
-        }
+				// Close the socket
+				try
+				{
+					socket.close();
+				}
+				catch (IOException e2)
+				{
+					// ErrorReporter.getInstance().handleException(e2);
+					Log.e(TAG, "unable to close() socket during connection failure", e2);
+				}
+			}
 
-        Message msg = handler.obtainMessage(MSLoggerApplication.GOT_SIG, fingerprint);
-        handler.sendMessage(msg);
-        tearDown();
+			try
+			{
+				fingerprint = getFingerprint(socket, remote);
+				located = true;
+			}
+			catch (IOException e)
+			{
+				tearDown();
+				delay(1000);
+			}
+		}
 
-    }
+		Message msg = handler.obtainMessage(MSLoggerApplication.GOT_SIG, fingerprint);
+		handler.sendMessage(msg);
+		tearDown();
 
-    private void tearDown()
-    {
-        if (mmInStream != null)
-        {
-            try
-            {
-                mmInStream.close();
-            }
-            catch (IOException e)
-            {
-            }
-            mmInStream = null;
-        }
-        if (mmOutStream != null)
-        {
-            try
-            {
-                mmOutStream.close();
-            }
-            catch (IOException e)
-            {
-            }
-            mmOutStream = null;
-        }
+	}
 
-        if (socket != null)
-        {
-            try
-            {
-                socket.close();
-            }
-            catch (IOException e)
-            {
-            }
-            socket = null;
-        }
-    }
+	private void tearDown()
+	{
+		if (mmInStream != null)
+		{
+			try
+			{
+				mmInStream.close();
+			}
+			catch (IOException e)
+			{
+			}
+			mmInStream = null;
+		}
+		if (mmOutStream != null)
+		{
+			try
+			{
+				mmOutStream.close();
+			}
+			catch (IOException e)
+			{
+			}
+			mmOutStream = null;
+		}
 
-    private String getFingerprint(BluetoothSocket socket, BluetoothDevice remote) throws IOException
-    {
-        InputStream tmpIn = null;
-        OutputStream tmpOut = null;
-        sendStatus("Establishing connection");
+		if (socket != null)
+		{
+			try
+			{
+				socket.close();
+			}
+			catch (IOException e)
+			{
+			}
+			socket = null;
+		}
+	}
 
-        // Get the BluetoothSocket input and output streams
-        try
-        {
-            tmpIn = socket.getInputStream();
-            tmpOut = socket.getOutputStream();
-        }
-        catch (IOException e)
-        {
-            DebugLogManager.INSTANCE.logException(e);
-            Log.e(TAG, "temp sockets not created", e);
-        }
+	private String getFingerprint(BluetoothSocket socket, BluetoothDevice remote) throws IOException
+	{
+		InputStream tmpIn = null;
+		OutputStream tmpOut = null;
+		sendStatus("Establishing connection");
 
-        mmInStream = tmpIn;
-        mmOutStream = tmpOut;
+		// Get the BluetoothSocket input and output streams
+		try
+		{
+			tmpIn = socket.getInputStream();
+			tmpOut = socket.getOutputStream();
+		}
+		catch (IOException e)
+		{
+			DebugLogManager.INSTANCE.logException(e);
+			Log.e(TAG, "temp sockets not created", e);
+		}
 
-        return fingerprint();
+		mmInStream = tmpIn;
+		mmOutStream = tmpOut;
 
-    }
+		return fingerprint();
 
-    private String fingerprint() throws IOException
-    {
-        sendStatus("Probing the ECU");
-        byte[] probeCommand1 = { 'Q' };
-        byte[] probeCommand2 = { 'S' };
-        byte[] bootCommand   = { 'X' };
-        int i = 0;
-        String sig = UNKNOWN;
+	}
 
-        while (i++ < 20)
-        {
-            byte[] response = writeAndRead(probeCommand1, 500);
+	private String fingerprint() throws IOException
+	{
+		sendStatus("Probing the ECU");
+		byte[] probeCommand1 = { 'Q' };
+		byte[] probeCommand2 = { 'S' };
+		byte[] bootCommand = { 'X' };
+		int i = 0;
+		String sig = UNKNOWN;
 
-            try
-            {
-                if (response != null && response.length > 1)
-                {
-                    sig = processResponse(response);
-                }
-                else
-                {
-                    response = writeAndRead(probeCommand2, 500);
-                    if (response != null && response.length > 1)
-                    {
-                        sig = processResponse(response);
-                    }
-                }
-                if (!UNKNOWN.equals(sig))
-                    break;
-            }
-            catch (BootException e)
-            {
-                response = writeAndRead(bootCommand, 500);
-            }
-        }
-        sendStatus(sig);
-        return sig;
-    }
+		while (i++ < 20)
+		{
+			byte[] response = writeAndRead(probeCommand1, 500);
 
-    private byte[] writeAndRead(byte[] probeCommand1, int d) throws IOException
-    {
-        sendStatus("Flushing streams");
-        mmOutStream.flush();
-        while (mmInStream.available() > 0)
-        {
-            mmInStream.read();
-        }
-        sendStatus("Sending "+new String(probeCommand1));
-        mmOutStream.write(probeCommand1);
-        delay(d);
+			try
+			{
+				if (response != null && response.length > 1)
+				{
+					sig = processResponse(response);
+				}
+				else
+				{
+					response = writeAndRead(probeCommand2, 500);
+					if (response != null && response.length > 1)
+					{
+						sig = processResponse(response);
+					}
+				}
+				if (!UNKNOWN.equals(sig))
+					break;
+			}
+			catch (BootException e)
+			{
+				response = writeAndRead(bootCommand, 500);
+			}
+		}
+		sendStatus(sig);
+		return sig;
+	}
 
-        if (mmInStream.available() == 0)
-        {
-            sendStatus("Didn't receive anything");
-            return null;
-        }
-        List<Byte> read = new ArrayList<Byte>();
+	private byte[] writeAndRead(byte[] probeCommand1, int d) throws IOException
+	{
+		sendStatus("Flushing streams");
+		mmOutStream.flush();
+		while (mmInStream.available() > 0)
+		{
+			mmInStream.read();
+		}
+		sendStatus("Sending " + new String(probeCommand1));
+		mmOutStream.write(probeCommand1);
+		delay(d);
 
-        while (mmInStream.available() > 0)
-        {
-            byte b = (byte) mmInStream.read();
-            read.add(b);
-        }
-        byte[] result = new byte[read.size()];
-        int i = 0;
-        for (Byte b : read)
-        {
-            result[i++] = b;
-        }
-        sendStatus("Recieved '"+new String(result)+"'");
-        return result;
-    }
+		if (mmInStream.available() == 0)
+		{
+			sendStatus("Didn't receive anything");
+			return null;
+		}
+		List<Byte> read = new ArrayList<Byte>();
 
-    private void delay(int d)
-    {
-        try
-        {
-            Thread.sleep(d);
-        }
-        catch (InterruptedException e)
-        {
-            //Swallow
-        }
-    }
+		while (mmInStream.available() > 0)
+		{
+			byte b = (byte) mmInStream.read();
+			read.add(b);
+		}
+		byte[] result = new byte[read.size()];
+		int i = 0;
+		for (Byte b : read)
+		{
+			result[i++] = b;
+		}
+		sendStatus("Recieved '" + new String(result) + "'");
+		return result;
+	}
 
-    private String processResponse(byte[] response) throws BootException
-    {
-        String result = new String(response);
-        if (result != null)
-        {
-            sendStatus("Got a signature of '" + result + "'");
-        }
-        if (result.contains("Boot>"))
-        {
-            throw new BootException();
-        }
-        if (response == null)
-            return UNKNOWN;
+	private void delay(int d)
+	{
+		try
+		{
+			Thread.sleep(d);
+		}
+		catch (InterruptedException e)
+		{
+			// Swallow
+		}
+	}
 
-        if (response.length == 1 && response[0] != 20)
-            return UNKNOWN;
+	private String processResponse(byte[] response) throws BootException
+	{
+		String result = new String(response);
+		if (result != null)
+		{
+			sendStatus("Got a signature of '" + result + "'");
+		}
+		if (result.contains("Boot>"))
+		{
+			throw new BootException();
+		}
+		if (response == null)
+			return UNKNOWN;
 
-        if ((response[0] != 'M' && response[0] != 'J') || (response[1] != 'S' && response[1] != 'o' && response[1] != 'i'))
-            return UNKNOWN;
+		if (response.length == 1 && response[0] != 20)
+			return UNKNOWN;
 
-        return result;
-    }
+		if ((response[0] != 'M' && response[0] != 'J') || (response[1] != 'S' && response[1] != 'o' && response[1] != 'i'))
+			return UNKNOWN;
 
-    @SuppressWarnings("serial")
-    private class BootException extends Exception
-    {
+		return result;
+	}
 
-    }
+	@SuppressWarnings("serial")
+	private class BootException extends Exception
+	{
+
+	}
 }
