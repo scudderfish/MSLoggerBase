131f67cbddb9c9401e6868109d905707f4c8c2da
Sebastien Giroux <sebastien@bolidea.com>
2012-06-12 19:40:25 -0400

    Avoid NPE on device without bluetooth (AKA android emulator)

--- src/uk/org/smithfamily/mslogger/widgets/BluetoothDevicePreference.java
+++ src/uk/org/smithfamily/mslogger/widgets/BluetoothDevicePreference.java
@@ -17,18 +17,21 @@ public class BluetoothDevicePreference extends ListPreference
         super(context, attrs);
 
         BluetoothAdapter bta = BluetoothAdapter.getDefaultAdapter();
-        Set<BluetoothDevice> pairedDevices = bta.getBondedDevices();
-        CharSequence[] entries = new CharSequence[pairedDevices.size()];
-        CharSequence[] entryValues = new CharSequence[pairedDevices.size()];
-        int i = 0;
-        for (BluetoothDevice dev : pairedDevices)
+        if (bta != null)
         {
-            entries[i] = dev.getName();
-            entryValues[i] = dev.getAddress();
-            i++;
+	        Set<BluetoothDevice> pairedDevices = bta.getBondedDevices();
+	        CharSequence[] entries = new CharSequence[pairedDevices.size()];
+	        CharSequence[] entryValues = new CharSequence[pairedDevices.size()];
+	        int i = 0;
+	        for (BluetoothDevice dev : pairedDevices)
+	        {
+	            entries[i] = dev.getName();
+	            entryValues[i] = dev.getAddress();
+	            i++;
+	        }
+	        setEntries(entries);
+	        setEntryValues(entryValues);
         }
-        setEntries(entries);
-        setEntryValues(entryValues);
     }
 
     public BluetoothDevicePreference(Context context)
