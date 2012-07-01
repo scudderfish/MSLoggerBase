bbf1fe51e8ccbfece5f8cfef628e2a51a56b8d20
Dave Smith <dave.g.smith@gmail.com>
2012-07-01 10:59:52 +0100

    Placeholder class for #20

--- src/uk/org/smithfamily/mslogger/widgets/BarMeter.java
+++ src/uk/org/smithfamily/mslogger/widgets/BarMeter.java
@@ -7,7 +7,7 @@ import android.view.View;
 /**
  *
  */
-public class BarMeter extends View
+public class BarMeter extends View implements Indicator
 {
 
     /**
@@ -19,6 +19,111 @@ public class BarMeter extends View
     {
         super(context, attrs);
         // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void setName(String name)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setChannel(String channelName)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setTitle(String title)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setUnits(String units)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setMin(float min)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setMax(float max)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setLowD(float lowD)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setLowW(float lowW)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setHiW(float hiW)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setHiD(float hiD)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setVD(int vd)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setLD(int ld)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setCurrentValue(double value)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public String getChannel()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void setDisabled(boolean disabled)
+    {
+        // TODO Auto-generated method stub
+        
     }
 
 }
