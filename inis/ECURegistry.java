945ef212a67adc3cc538c734317b519fdf18e4b5
Dave Smith <dave.g.smith@gmail.com>
2012-07-01 12:36:20 +0100

    Fuzzy matching on unknown firmwares

--- inis/ECURegistry.java
+++ inis/ECURegistry.java
@@ -20,32 +20,54 @@ public enum ECURegistry
 <LIST>
     }
 
-    private void registerEcu(Class<? extends Megasquirt> cls, String sig)
-    {
-        ecus.put(sig, cls);
-    }
-    
-    public synchronized Megasquirt getECU(String sig,Context ctx)
-    {
-        Megasquirt ecu = instances.get(sig);
-        if(ecu != null)
-        {
-            return ecu;
-        }
-        Class<? extends Megasquirt> c = ecus.get(sig);
-        
-        try
-        {
-            Constructor<? extends Megasquirt> constructor = c.getConstructor(Context.class);
-            ecu = constructor.newInstance(ctx);
-            instances.put(sig, ecu);
-        }
-        catch (Exception e)
-        {
-            // EEP!
-            e.printStackTrace();
-        }
-        
-        return ecu;
-    }
+	private void registerEcu(Class<? extends Megasquirt> cls, String sig)
+	{
+		ecus.put(sig, cls);
+	}
+
+	public synchronized Megasquirt getECU(String sig, Context ctx)
+	{
+		Megasquirt ecu = instances.get(sig);
+		if (ecu != null)
+		{
+			return ecu;
+		}
+		Class<? extends Megasquirt> c = findEcu(sig);
+
+		try
+		{
+			Constructor<? extends Megasquirt> constructor = c
+					.getConstructor(Context.class);
+			ecu = constructor.newInstance(ctx);
+			instances.put(sig, ecu);
+		} catch (Exception e)
+		{
+			// EEP!
+			e.printStackTrace();
+		}
+
+		return ecu;
+	}
+
+	private Class<? extends Megasquirt> findEcu(String sig)
+	{
+		Class<? extends Megasquirt> ecu = ecus.get(sig);
+		if (ecu != null)
+		{
+			return ecu;
+		}
+		for (int i = sig.length() - 1; i > sig.length() / 2 && i > 3
+				&& ecu == null; i--)
+		{
+			String fuzzySig = sig.substring(0, i);
+			for (String classSig : ecus.keySet())
+			{
+				if (classSig.startsWith(fuzzySig))
+				{
+					ecu = ecus.get(classSig);
+				}
+			}
+		}
+		return ecu;
+	}
 }
