f86c19d6571872cab6ca951321e81facad45c2d4
Sebastien Giroux <sebastien@bolidea.com>
2012-07-10 00:52:14 -0400

    GaugeDetails type and orientation added

--- dummy_src/uk/org/smithfamily/mslogger/widgets/GaugeDetails.java
+++ dummy_src/uk/org/smithfamily/mslogger/widgets/GaugeDetails.java
@@ -2,7 +2,7 @@ package uk.org.smithfamily.mslogger.widgets;
 public class GaugeDetails
 {
 
-	public GaugeDetails(String string, String string2,double d, String string3, String string4, double i, double j, double k, double l, double m, double n,
+	public GaugeDetails(String string, String string2, String string3, String string4,double d, String string5, String string6, double i, double j, double k, double l, double m, double n,
 			double o, double p, double q)
 	{
 		// TODO Auto-generated constructor stub
