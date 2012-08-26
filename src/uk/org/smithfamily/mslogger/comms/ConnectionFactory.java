852275e4d6230df024361f1bff2ff5e3420a8307
Dave Smith <dave.g.smith@gmail.com>
2012-08-26 20:20:59 +0100

    Revert to a Bluetooth connection.

--- src/uk/org/smithfamily/mslogger/comms/ConnectionFactory.java
+++ src/uk/org/smithfamily/mslogger/comms/ConnectionFactory.java
@@ -12,7 +12,7 @@ public enum ConnectionFactory
      */
     public Connection getConnection()
     {
-        return SocketConnection.INSTANCE;
+        return BluetoothConnection.INSTANCE;
     }
     
 }
