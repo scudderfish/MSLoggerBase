59fde10cd5a099a80ad0c9e1a5a6e60c85976f13
Dave Smith <dave.g.smith@gmail.com>
2012-09-20 14:11:49 +0100

    Let a setting know which group it belongs to

--- common_src/uk/org/smithfamily/mslogger/ecuDef/SettingGroup.java
+++ common_src/uk/org/smithfamily/mslogger/ecuDef/SettingGroup.java
@@ -6,17 +6,19 @@ import java.util.List;
 public class SettingGroup
 {
 
-    private String name;
-    private String description;
+    private String              name;
+    private String              description;
     private List<SettingOption> options = new ArrayList<SettingOption>();
 
     public class SettingOption
     {
-        private String flag;
-        private String description;
+        private String       flag;
+        private String       description;
+        private SettingGroup group;
 
-        public SettingOption(String flag,String description)
+        public SettingOption(SettingGroup group, String flag, String description)
         {
+            this.group = group;
             this.flag = flag;
             this.description = description;
         }
@@ -30,27 +32,43 @@ public class SettingGroup
         {
             return description;
         }
+
+        public SettingGroup getGroup()
+        {
+            return group;
+        }
+
+        @Override
+        public String toString()
+        {
+            return description;
+        }
     }
-    public SettingGroup(String name,String description)
+
+    public SettingGroup(String name, String description)
     {
         this.name = name;
         this.description = description;
     }
+
     public String getName()
     {
         return name;
     }
+
     public String getDescription()
     {
         return description;
     }
-    public void addOption(String flag,String description)
+
+    public void addOption(String flag, String description)
     {
-        SettingOption option = new SettingOption(flag,description);
+        SettingOption option = new SettingOption(this, flag, description);
         options.add(option);
     }
+
     public List<SettingOption> getOptions()
     {
         return options;
     }
- }
+}
