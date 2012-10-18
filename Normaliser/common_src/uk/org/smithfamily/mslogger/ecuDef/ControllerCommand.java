5cd0e5c05ab3a7e4d86c2f1f17378f8c136f34d8
Sebastien Giroux <sebastien@bolidea.com>
2012-10-17 22:04:17 -0400

    Support for new MS3 ini section ControllerCommands + Added support for MS3 pre1.1 alpha 2 + ini typo fixes

---
+++ Normaliser/common_src/uk/org/smithfamily/mslogger/ecuDef/ControllerCommand.java
@@ -0,0 +1,39 @@
+package uk.org.smithfamily.mslogger.ecuDef;
+
+public class ControllerCommand
+{
+    private String name = "";
+    private String command = "";
+    
+    public ControllerCommand(String name, String command)
+    {
+        this.name = name;
+        this.command = command;
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    
+    public String getCommand()
+    {
+        return command;
+    }
+    
+    public void setCommand(String command)
+    {
+        this.command = command;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ControllerCommand [name=" + name + ", command=" + command + "]";
+    }
+}
