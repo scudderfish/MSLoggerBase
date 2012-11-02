ac22c5cc30696b126e88f220d37d62fd40899bba
Dave Smith <dave.g.smith@gmail.com>
2012-11-02 13:10:37 +0000

    Do dev work against the simulator as I'm away from home ATM

--- MSLogger/src/uk/org/smithfamily/mslogger/comms/ConnectionFactory.java
+++ MSLogger/src/uk/org/smithfamily/mslogger/comms/ConnectionFactory.java
@@ -5,14 +5,15 @@ public enum ConnectionFactory
     INSTANCE;
 
     /**
-     * Return the Connection instance to use.  This should be switchable somehow
-     * but as I'm in a hotel, I'm hardwiring it to a simulator as my ECU is >1000 miles
-     * away
+     * Return the Connection instance to use. This should be switchable somehow but as I'm in a hotel, I'm hardwiring it to a simulator as my ECU is
+     * >1000 miles away
+     * 
      * @return
      */
     public Connection getConnection()
     {
-        return new BluetoothConnection();
+        // return new BluetoothConnection();
+        return SocketConnection.INSTANCE;
     }
-    
+
 }
